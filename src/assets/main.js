716733d05f3ce9620a4b6cd89d81cd6b8f724728
Herman Owens <herman.owens3@gmail.com>
2016-12-31 11:45:40 -0500

    Updates the project to pull my code school badges via jQuery.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,30 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/hermanowens3.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      if (!response || !response.courses || !response.courses.completed) {
+        return false;
+      }
 
+      var completed =  response.courses.completed;
+
+      $('#badges').html(
+        $.map(completed, function (course) {
+          var html = '';
+
+          html += '<div class="course">';
+          html += '<h3>' + course.title + '</h3>';
+          html += '<img src="' + course.badge + '" title="' + course.title + '" />';
+          html += '<a href="' + course.url + '" target="_blank" class="btn btn-primary">See Course</a>';
+          html += '</div>';
+
+          return html;
+        })
+      );
+    }
+  });
 });
